e04b5e8303353191bd8bcb7b756cae3c9e968f87
Lukas Welte <ich@lukaswelte.de>
2014-05-14 18:58:52 +0200

    Updated build file

---
+++ src/Main/helper/PermutationsTest.java
@@ -0,0 +1,19 @@
+package Main.helper;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class PermutationsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
